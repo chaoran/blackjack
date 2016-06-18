9bc6661eb135751f800a70b4ea0cf16875e450ac
Chaoran Yang <chaorany@me.com>
2016-06-18 18:49:46 -0500

    [NF] Make 'gulp test' depends on 'gulp jshint'.

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,16 @@ var stylish = require('jshint-stylish');
 gulp.task('jshint', function() {
   return gulp.src(['*.js', 'lib/*.js', 'spec/*.js']).
     pipe(jshint()).
-    pipe(jshint.reporter(stylish));
+    pipe(jshint.reporter(stylish)).
+    pipe(jshint.reporter('fail'));
 });
 
 var jasmine = require('gulp-jasmine');
 
-gulp.task('test', function() {
+gulp.task('test', [ 'jshint' ], function() {
   return gulp.src('spec/*.js').
     pipe(jasmine());
-});
+})
 
 gulp.task('default', [ 'jshint', 'test' ]);
 
